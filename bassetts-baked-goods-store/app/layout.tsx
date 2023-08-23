ca61572b713991a9f35f80c8a5d3fc69b0950676
tlemick <tlemick@gmail.com>
2023-08-22 17:18:41 -0700

    Initial commit

---
+++ bassetts-baked-goods-store/app/layout.tsx
@@ -0,0 +1,32 @@
+import type { Metadata } from "next"
+import { Urbanist } from "next/font/google"
+
+import Footer from "@/components/footer"
+import Navbar from "@/components/navbar"
+import ModalProvider from "@/providers/modal-provider"
+
+import "./globals.css"
+
+const font = Urbanist({ subsets: ["latin"] })
+
+export const metadata: Metadata = {
+    title: "Store",
+    description: "A Store",
+}
+
+export default function RootLayout({
+    children,
+}: {
+    children: React.ReactNode
+}) {
+    return (
+        <html lang="en">
+            <body className={font.className}>
+                <ModalProvider />
+                <Navbar />
+                {children}
+                <Footer />
+            </body>
+        </html>
+    )
+}
