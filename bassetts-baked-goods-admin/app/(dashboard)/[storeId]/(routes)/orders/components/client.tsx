e30aa7579633ea2f4c597bf606a73d4a14948f36
tlemick <tlemick@gmail.com>
2023-08-28 16:26:47 -0700

    Added loading state for admin page

--- bassetts-baked-goods-admin/app/(dashboard)/[storeId]/(routes)/orders/components/client.tsx
+++ bassetts-baked-goods-admin/app/(dashboard)/[storeId]/(routes)/orders/components/client.tsx
@@ -2,8 +2,9 @@
 
 import { Heading } from "@/components/ui/heading"
 import { Separator } from "@/components/ui/separator"
-import { OrderColumn, columns } from "./columns"
 import { DataTable } from "@/components/ui/data-table"
+
+import { OrderColumn, columns } from "./columns"
 
 interface OrderClientProps {
     data: OrderColumn[]
